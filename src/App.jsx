7050defb1308b9aafeef3dedd7e8871eceb6f562
vladlen01~ <vladlenkuznetsov203@gmail.com>
2022-03-26 16:55:00 +0100

    init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Post from "./components/post/Post";
+
+function App() {
+
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
