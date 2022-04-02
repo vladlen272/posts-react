9242f3df105e393776ef510232fbf46740f477d5
vladlen01~ <vladlenkuznetsov203@gmail.com>
2022-04-02 19:38:12 +0200

    added the ability to search

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import "./styles/App.css";
 import Posts from "./components/post-list/PostList";
 import AddPostForm from "./components/add-post-form/AddPostForm";
 import Select from "./components/UI/select/Select";
+import Input from "./components/UI/input/Input";
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "js", body: "js is better" },
@@ -11,10 +12,27 @@ function App() {
     { id: 4, title: "redux", body: "i dont know bro" },
   ]);
 
-  const [selectedSort, setSelectedSort] = useState('')
+  const [selectedSort, setSelectedSort] = useState("");
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const sortedPosts = useMemo(() => {
+    if (selectedSort) {
+      return [...posts].sort((a, b) =>
+        a[selectedSort].localeCompare(b[selectedSort])
+      );
+    }
+    return posts;
+  }, [selectedSort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }, [searchQuery, sortedPosts]);
 
   const createPost = (newPost) => {
-    const newPostId = posts.length + 1;
+    const newPostId = Date.now();
     const post = { ...newPost, id: newPostId };
     setPosts([...posts, post]);
   };
@@ -24,17 +42,38 @@ function App() {
   };
 
   const sortPosts = (sort) => {
-    setSelectedSort(sort)
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
-  }
-
-
+    setSelectedSort(sort);
+  };
 
   return (
     <div className="App">
       <AddPostForm create={createPost} />
-      <Select onChange={sortPosts} value={selectedSort} defaultValue={'select'} options={[{value: 'title', name: 'title'}, {value: 'body', name: 'body'}]}/>
-      <Posts remove={removePost} posts={posts} title="Posts about js" />
+      <hr
+        style={{
+          margin: "15px 0",
+          border: "0.1px solid blue",
+          backgroundColor: "blue",
+        }}
+      />
+      <Input
+        placeholder="search"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      ></Input>
+      <Select
+        onChange={sortPosts}
+        value={selectedSort}
+        defaultValue={"select"}
+        options={[
+          { value: "title", name: "title" },
+          { value: "body", name: "body" },
+        ]}
+      />
+      <Posts
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title="Posts about js"
+      />
     </div>
   );
 }
--- src/components/UI/select/Select.jsx
+++ src/components/UI/select/Select.jsx
@@ -4,17 +4,20 @@ import classes from "./select.module.css";
 const Select = ({ defaultValue, options, value, onChange }) => {
   return (
     <select
-    value={value}
-    onChange={event => onChange(event.target.value)}
+      className={classes.select}
+      value={value}
+      onChange={(event) => onChange(event.target.value)}
     >
       <option disabled value="">
         {defaultValue}
       </option>
-      {options.map((option) => 
-        <option key={option.value} value={option.value}>{option.name}</option>
-        )}
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.name}
+        </option>
+      ))}
     </select>
   );
-}
+};
 
 export default Select;
